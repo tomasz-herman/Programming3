531df1c0d1f648c5f076cd01b562807c90e46c80
Tomasz Herman <tomasz.herman@protonmail.com>
2024-11-03 23:00:50 +0100

    Lambdas and delegates

---
+++ Advanced/Delegates/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+
+namespace Delegates;
+
+class Program
+{
+    // Delegate type declaration:
+    public delegate double Function(double x);
+    
+    static void Main(string[] args)
+    {
+        DelegatesIntroduction();
+        GenericDelegates();
+        BuiltinDelegates();
+        MulticastDelegates();
+    }
+
+    private static void DelegatesIntroduction()
+    {
+        PrintCurrentMethodName();
+        Function func = Square;
+        // Equivalent:
+        // Function func = new Function(Square);
+        double result = func(3);
+        // Equivalent:
+        // double result = func.Invoke(3);
+        Console.WriteLine(result);
+    }
+    
+    public static double Square(double x) => x * x;
+
+    public delegate T GenericFunction<T>(T x);
+
+    private static void GenericDelegates()
+    {
+        PrintCurrentMethodName();
+        GenericFunction<double> func = Square;
+        double result = func(3);
+        Console.WriteLine(result);
+    }
+
+    private static void BuiltinDelegates()
+    {
+        PrintCurrentMethodName();
+        Func<double, double> func = Square;
+        double result = func(3);
+        Action<double> write = Console.WriteLine;
+        write(result);
+    }
+    
+    private static void MulticastDelegates()
+    {
+        PrintCurrentMethodName();
+        Action<string> writeLog = null!;
+        writeLog += Console.WriteLine;
+        writeLog += WriteLogToFile;
+        writeLog("DEBUG: This is a test log");
+    }
+
+    private static void WriteLogToFile(string log)
+    {
+        File.WriteAllText("test.log", log);
+    }
+    
+    private static void PrintCurrentMethodName()
+    {
+        var st = new StackTrace();
+        var sf = st.GetFrame(1);
+
+        Console.WriteLine("***************************************");
+        Console.WriteLine($"* Method: {sf!.GetMethod()!.Name,27} *");
+        Console.WriteLine("***************************************");
+    }
+}
---
+++ Advanced/Lamdas/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+namespace Lamdas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LambdasDeclaration();
+        ExplicitParameterAndReturn();
+        DefaultParameters();
+        CapturingVariables();
+    }
+
+    private static void LambdasDeclaration()
+    {
+        PrintCurrentMethodName();
+        // Can skip parenthesis if lambda takes only 1 parameter:
+        Func<double, double> square = x => x * x;
+        Func<string> greet = () => "Hello, world";
+        Func<char, int, string> repeat = (c, i) => new string(c, i);
+        Action<string> write = str => Console.Write(str);
+    }
+    
+    private static void ExplicitParameterAndReturn()
+    {
+        PrintCurrentMethodName();
+        Func<double, double> square = double (double x) => x * x;
+        Func<string> greet = string () => "Hello, world";
+        Func<char, int, string> repeat = string (char c, int i) => new string(c, i);
+        Action<string> write = void (string str) => Console.Write(str);
+    }
+    
+    private static void DefaultParameters()
+    {
+        PrintCurrentMethodName();
+        var write = (string str = "hello") => Console.Write(str);
+        write();
+        write("world");
+        Console.WriteLine();
+    }
+
+    private static void CapturingVariables()
+    {
+        PrintCurrentMethodName();
+        Action[] actions = new Action[3];
+        for (int i = 0; i < 3; i++)
+            actions [i] = () => Console.Write(i);
+        foreach (Action action in actions) action();
+    }
+    
+    private static void PrintCurrentMethodName()
+    {
+        var st = new StackTrace();
+        var sf = st.GetFrame(1);
+
+        Console.WriteLine("***************************************");
+        Console.WriteLine($"* Method: {sf!.GetMethod()!.Name,27} *");
+        Console.WriteLine("***************************************");
+    }
+}
