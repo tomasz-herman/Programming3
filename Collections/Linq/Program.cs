f649dbaa4697eaf6c999c16c342c1f71f3347848
Tomasz Herman <tomasz.herman@protonmail.com>
2024-11-04 00:03:42 +0100

    Linq

--- Collections/Linq/Program.cs
+++ Collections/Linq/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] names = ["Tom", "Dick", "Harry", "Mary", "Jay"];
+        
+        IEnumerable<string> filteredNames = names
+            .Where(n => n.Length >= 4);
+        foreach (string name in filteredNames) 
+            Console.WriteLine(name);
+
+        Console.WriteLine();
+        
+        IEnumerable<string> query = names
+            .Where(n => n.Contains("a"))
+            .OrderBy(n => n.Length)
+            .Select(n => n.ToUpper());
+        // Query is not executed, unless enumerated:
+        foreach (var name in query)
+        {
+            Console.WriteLine(name);
+        }
     }
 }
