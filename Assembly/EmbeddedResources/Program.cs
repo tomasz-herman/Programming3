14a7d6438954e3ae8f5a814526d5a0027826d7af
Tomasz Herman <tomasz.herman@protonmail.com>
2024-12-01 17:31:41 +0100

    Embedded resources

---
+++ Assembly/EmbeddedResources/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+
+namespace EmbeddedResources;
+
+class Program
+{
+    static void Main()
+    {
+        Assembly? a = Assembly.GetEntryAssembly();
+        using (Stream? s = a?.GetManifestResourceStream("EmbeddedResources.lorem.txt"))
+        {
+            if (s == null) throw new MissingManifestResourceException("Resource not found");
+            StreamReader sr = new StreamReader(s);
+            Console.WriteLine(sr.ReadLine());
+        }
+    }
+}
