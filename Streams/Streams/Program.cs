4fae3fb7c66a78abeccf6907a03fe7842d59e61a
Tomasz Herman <tomasz.herman@protonmail.com>
2024-12-01 21:19:55 +0100

    Basic stream demo

---
+++ Streams/Streams/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Streams;
+
+class Program
+{
+    static void Main()
+    {
+        // Create a file called test.txt in the current directory:
+        using Stream stream = new FileStream("test.txt", FileMode.Create);
+        Console.WriteLine($"Can read: {stream.CanRead}");
+        Console.WriteLine($"Can write: {stream.CanWrite}");
+        Console.WriteLine($"Can Seek: {stream.CanSeek}");
+
+        stream.WriteByte(101);
+        stream.WriteByte(102);
+        byte[] block = [1, 2, 3, 4, 5];
+        stream.Write (block, 0, block.Length);
+
+        Console.WriteLine($"Stream length: {stream.Length}");
+        Console.WriteLine($"Stream position: {stream.Position}");
+        stream.Position = 0;
+
+        Console.WriteLine($"First byte: {stream.ReadByte()}");
+        Console.WriteLine($"Second byte: {stream.ReadByte()}");
+        Console.WriteLine($"Reading {block.Length} bytes, twice:");
+        Console.WriteLine($"Read: {stream.Read(block, 0, block.Length)} bytes");
+        Console.WriteLine($"Read {stream.Read(block, 0, block.Length)} bytes");
+    }
+}
