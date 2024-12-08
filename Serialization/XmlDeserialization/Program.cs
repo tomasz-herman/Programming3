81613e376ee4efca83d6e037837043177da1cab4
Tomasz Herman <tomasz.herman@protonmail.com>
2024-12-08 21:00:53 +0100

    Xml serialization examples

---
+++ Serialization/XmlDeserialization/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlDeserialization;
+
+public class WeatherForecast
+{
+  public DateTime Date { get; set; }
+  public int TemperatureCelsius { get; set; }
+  public string? Summary { get; set; }
+  public string? SummaryField;
+  public List<DateTime>? DatesAvailable { get; set; }
+  public SerializableDictionary<string, HighLowTemps>? TemperatureRanges { get; set; }
+  public string[]? SummaryWords { get; set; }
+}
+
+public class HighLowTemps
+{
+  public int High { get; set; }
+  public int Low { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string xml = """
+                     <?xml version="1.0" encoding="utf-16"?>
+                     <WeatherForecast xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+                       <SummaryField>Hot</SummaryField>
+                       <Date>2019-08-01T00:00:00</Date>
+                       <TemperatureCelsius>25</TemperatureCelsius>
+                       <Summary>Hot</Summary>
+                       <DatesAvailable>
+                         <dateTime>2019-08-01T00:00:00</dateTime>
+                         <dateTime>2019-08-02T00:00:00</dateTime>
+                       </DatesAvailable>
+                       <TemperatureRanges>
+                         <item>
+                           <key>
+                             <string>Cold</string>
+                           </key>
+                           <value>
+                             <HighLowTemps>
+                               <High>20</High>
+                               <Low>-10</Low>
+                             </HighLowTemps>
+                           </value>
+                         </item>
+                         <item>
+                           <key>
+                             <string>Hot</string>
+                           </key>
+                           <value>
+                             <HighLowTemps>
+                               <High>60</High>
+                               <Low>20</Low>
+                             </HighLowTemps>
+                           </value>
+                         </item>
+                       </TemperatureRanges>
+                       <SummaryWords>
+                         <string>Cool</string>
+                         <string>Windy</string>
+                         <string>Humid</string>
+                       </SummaryWords>
+                     </WeatherForecast>
+                     
+                     """;
+        
+        var stringReader = new StringReader(xml);
+        var xmlSerializer = new XmlSerializer(typeof(WeatherForecast));
+        WeatherForecast? wf = (WeatherForecast?)xmlSerializer.Deserialize(stringReader);
+
+        Console.WriteLine($"Date: {wf?.Date}");
+        Console.WriteLine($"TemperatureCelsius: {wf?.TemperatureCelsius}");
+        Console.WriteLine($"Summary: {wf?.Summary}");
+    }
+}
---
+++ Serialization/XmlSerialization/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml.Serialization;
+
+namespace XmlSerialization;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+    public int TemperatureCelsius { get; set; }
+    public string? Summary { get; set; }
+    public string? SummaryField;
+    public List<DateTime>? DatesAvailable { get; set; }
+    public SerializableDictionary<string, HighLowTemps>? TemperatureRanges { get; set; }
+    public string[]? SummaryWords { get; set; }
+}
+
+public class HighLowTemps
+{
+    public int High { get; set; }
+    public int Low { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var weatherForecast = new WeatherForecast
+        {
+            Date = DateTime.Parse("2019-08-01"),
+            TemperatureCelsius = 25,
+            Summary = "Hot",
+            SummaryField = "Hot",
+            DatesAvailable = new List<DateTime>() 
+                { DateTime.Parse("2019-08-01"), DateTime.Parse("2019-08-02") },
+            TemperatureRanges = new SerializableDictionary<string, HighLowTemps>
+            {
+                ["Cold"] = new HighLowTemps { High = 20, Low = -10 },
+                ["Hot"] = new HighLowTemps { High = 60 , Low = 20 }
+            },
+            SummaryWords = new[] { "Cool", "Windy", "Humid" }
+        };
+
+        var stringWriter = new StringWriter();
+        var xmlSerializer = new XmlSerializer(typeof(WeatherForecast));
+        xmlSerializer.Serialize(stringWriter, weatherForecast);
+
+        Console.WriteLine(stringWriter.ToString());
+    }
+}
