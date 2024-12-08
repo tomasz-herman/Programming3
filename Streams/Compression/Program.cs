28af026e2fd5ce3ae2462dd860ebdfe2f93fd653
Tomasz Herman <tomasz.herman@protonmail.com>
2024-12-08 16:31:43 +0100

    Compression example

---
+++ Streams/Compression/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO.Compression;
+
+namespace Compression;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Compress("lorem.txt", "lorem.txt.compressed");
+        Decompress("lorem.txt.compressed", "decompressed_lorem.txt");
+    }
+
+    static void Compress(string input, string output)
+    {
+        using var fsIn = File.OpenRead(input);
+        using var fsOut = File.Create(output);
+        using var ds = new GZipStream(fsOut, CompressionMode.Compress);
+        
+        byte[] buffer = new byte[4096];
+        int read;
+
+        while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            ds.Write(buffer, 0, read);
+        }
+    }
+
+    static void Decompress(string input, string output)
+    {
+        using var fsIn = File.OpenRead(input);
+        using var ds = new GZipStream(fsIn, CompressionMode.Decompress);
+        using var fsOut = File.Create(output);
+
+        byte[] buffer = new byte[4096];
+        int read;
+
+        while ((read = ds.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            fsOut.Write(buffer, 0, read);
+        }
+    }
+}
---
+++ Streams/ZipBomb/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ZipBomb;
+
+class Program
+{
+    const int ChunkSize = 1024 * 1024 * 1024;
+    const int Count = 1024;
+    
+    static void Main(string[] args)
+    {
+        byte[] chunk = new byte[ChunkSize];
+        // TODO
+    }
+}
