0f47b5e825d602332dd0907b7e8399efe9c4caf7
Tomasz Herman <tomasz.herman@protonmail.com>
2025-01-07 03:04:01 +0100

    async/await threads demo

---
+++ Concurrency/AsyncAwaitThreads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using SynchronizationContexts;
+
+namespace AsyncAwaitThreads;
+
+class Program
+{
+    private static async Task Main()
+    {
+        Thread.CurrentThread.Name = "Main";
+        // SynchronizationContext.SetSynchronizationContext(new MySynchronizationContext());
+
+        PrintCurrentThread();
+
+        Task<string> task = HelloWorldAsync();
+
+        PrintCurrentThread();
+
+        string hello = await task;
+        
+        PrintCurrentThread();
+
+        Console.WriteLine(hello);
+    }
+
+    static async Task<string> HelloWorldAsync()
+    {
+        PrintCurrentThread();
+
+        await Task.Delay(3000);
+
+        PrintCurrentThread();
+        
+        return "Hello, World";
+    }
+
+    private static void PrintCurrentThread([CallerLineNumber] int line = -1)
+    {
+        var current = Thread.CurrentThread;
+        Console.WriteLine($"Thread(line {line}): {current.Name} : {current.ManagedThreadId}");
+    }
+}
