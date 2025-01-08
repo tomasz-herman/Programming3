667acfa355cefe2d1d6d5dfb5a6bfa5f362202a1
Tomasz Herman <tomasz.herman@protonmail.com>
2025-01-08 05:43:34 +0100

    Barrier and EventHandles examples

---
+++ Synchronization/BarrierExample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BarrierExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var barrier = new Barrier(3, _ => Console.WriteLine());
+        new Thread(RollDice).Start();
+        new Thread(RollDice).Start();
+        new Thread(RollDice).Start();
+        void RollDice()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                barrier.SignalAndWait();
+                Console.Write(D6() + " ");
+                barrier.SignalAndWait();
+            }
+        }
+    }
+
+    static int D6()
+    {
+        return 1 + Random.Shared.Next(6);
+    }
+}
