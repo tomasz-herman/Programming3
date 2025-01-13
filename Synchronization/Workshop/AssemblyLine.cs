14ab97134421d816ae6699a4d6f18d8ab3d00d05
Tomasz Herman <tomasz.herman@protonmail.com>
2025-01-13 16:40:30 +0100

    PL solution

--- Synchronization/Workshop/AssemblyLine.cs
+++ Synchronization/Workshop/AssemblyLine.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Workshop;
 
 public class AssemblyLine<T> : IAssemblyLine<T>
@@ -18,13 +20,99 @@ public class AssemblyLine<T> : IAssemblyLine<T>
         throw new NotImplementedException();
     }
 
-    public Task<T?> TryDequeueAsync(int timeoutMilliseconds, CancellationToken cancellationToken)
+    public async Task<T?> TryDequeueAsync(int timeoutMilliseconds, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 
-    public Task<bool> TryEnqueueAsync(T item, int timeoutMilliseconds, CancellationToken cancellationToken)
+    public async Task<bool> TryEnqueueAsync(T item, int timeoutMilliseconds, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class AssemblyLine<T> : IAssemblyLine<T>
+// {
+//     // TODO: Implement assembly line class in a thread-safe way.
+//     private List<T> Items { get; set; } = new List<T>();
+//     private SemaphoreSlim _itemSemaphore;
+//     private SemaphoreSlim _emptySemaphore;
+//     public AssemblyLine(int capacity)
+//     {
+//         _itemSemaphore = new SemaphoreSlim(0, capacity);
+//         _emptySemaphore = new SemaphoreSlim(capacity, capacity);
+//     }
+//     
+//     public T Dequeue()
+//     {
+//         _itemSemaphore.Wait();
+//         T ret;
+//         lock (Items)
+//         {
+//             ret = Items[0];
+//             Items.RemoveAt(0);
+//         }
+//         _emptySemaphore.Release();
+//         return ret;
+//     }
+// 
+//     public void Enqueue(T item)
+//     {
+//         _emptySemaphore.Wait();
+//         lock (Items)
+//             Items.Add(item);
+//         _itemSemaphore.Release();
+//     }
+// 
+//     public async Task<T?> TryDequeueAsync(int timeoutMilliseconds, CancellationToken cancellationToken)
+//     {
+//         try
+//         {
+//             await _itemSemaphore.WaitAsync(timeoutMilliseconds, cancellationToken);
+//         }
+//         catch (Exception)
+//         {
+//             return default;
+//         }
+//         T ret;
+//         lock (Items)
+//         {
+//             ret = Items[0];
+//             Items.RemoveAt(0);
+//         }
+//         _emptySemaphore.Release();
+//         return ret;
+//     }
+// 
+//     public async Task<bool> TryEnqueueAsync(T item, int timeoutMilliseconds, CancellationToken cancellationToken)
+//     {
+//         try
+//         {
+//             await _emptySemaphore.WaitAsync(timeoutMilliseconds, cancellationToken);
+//         }
+//         catch (Exception)
+//         {
+//             return false;
+//         }
+//         lock (Items)
+//             Items.Add(item);
+//         _itemSemaphore.Release();
+//         return true;
+//     }
+// }
--- Synchronization/Workshop/Workshop.cs
+++ Synchronization/Workshop/Workshop.cs
@@ -53,3 +53,115 @@ public class Workshop
         return Task.Run(() => {});
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// public class Workshop
+// {
+//     private const int AssemblyLineCapacity = 8;
+// 
+//     private Sleigh Sleigh { get; } = new();
+//     private AssemblyLine<Gift> AssemblyLine { get; } = new(AssemblyLineCapacity);
+//     private AutoResetEvent _deliverEvent = new(false);
+//     private AutoResetEvent _resumeWrappingEvent = new(false);
+//     public async Task Start(CancellationToken token)
+//     {
+//         Task[] assemblers = StartAssemblers(token);
+//         Task[] wrappers = StartWrappers(token);
+//         Task santa = StartSanta(token);
+//         
+//         await Task.WhenAll(assemblers);
+//         await Task.WhenAll(wrappers);
+//         await Task.WhenAll(santa);
+//     }
+//     
+//     /// <summary>
+//     /// Each Assembler takes a new Gift Order (new Gift()).
+//     /// Assembles the order, and puts it on the assembly line.
+//     /// </summary>
+//     private const int AssemblersCount = 5;
+//     private Task[] StartAssemblers(CancellationToken token)
+//     {
+//         Task[] tasks = new Task[AssemblersCount];
+//         for (int i = 0; i < AssemblersCount; i++)
+//         {
+//             tasks[i] = Task.Run(() =>
+//             {
+//                 while (!token.IsCancellationRequested)
+//                 {
+//                     Gift gift = new();
+//                     gift.Assemble();
+//                     AssemblyLine.Enqueue(gift);
+//                 }
+//             });
+//         }
+//         return tasks;
+//     }
+//     
+//     /// <summary>
+//     /// Each Wrapper takes a new Gift from assembly line.
+//     /// Wraps the order, and puts it into the Sleigh.
+//     /// Only one Elf at the time can load a present to the sleigh.
+//     /// When the sleigh is full they call the Santa to deliver.
+//     /// </summary>
+//     private const int WrappersCount = 3;
+//     private Task[] StartWrappers(CancellationToken token)
+//     {
+//         Task[] tasks = new Task[WrappersCount];
+//         for (int i = 0; i < WrappersCount; i++)
+//         {
+//             tasks[i] = Task.Run(() =>
+//             {
+//                 while (!token.IsCancellationRequested)
+//                 {
+//                     Gift gift = AssemblyLine.Dequeue();
+//                     gift.Wrap();
+//                     lock (Sleigh)
+//                     {
+//                         if (Sleigh.PushGift(gift) == false)
+//                         {
+//                             _deliverEvent.Set();
+//                             _resumeWrappingEvent.WaitOne();
+//                             Sleigh.PushGift(gift);
+//                         }
+//                     }
+//                 }
+//             });
+//         }
+//         return tasks;
+//     }
+//     
+//     /// <summary>
+//     /// When called by one of the wrapper to delivers goes out for the delivery.
+//     /// After he comes back he signals others to resume moving gifts to the sleigh,
+//     /// and waits again for another call for delivery.
+//     /// </summary>
+//     private Task StartSanta(CancellationToken token)
+//     {
+//         // TODO: Implement santa
+//         return Task.Run(() =>
+//         {
+//             while (token.IsCancellationRequested == false)
+//             {
+//                 _deliverEvent.WaitOne();
+//                 Sleigh.DeliverGifts();
+//                 _resumeWrappingEvent.Set();
+//             }
+//         });
+//     }
+// }
